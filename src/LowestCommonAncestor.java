f805af352afa427a37fdb02980d462a315e30a0c
Aruomeng <158143437+Aruomeng@users.noreply.github.com>
2024-01-30 18:49:01 +0800

    Update LowestCommonAncestor.java

--- src/LowestCommonAncestor.java
+++ src/LowestCommonAncestor.java
@@ -56,6 +56,7 @@ public class LowestCommonAncestor {
         TreeNode lca = lcaFinder.lowestCommonAncestor(root, p, q);
 
         System.out.println("The Lowest Common Ancestor of " + p.val + " and " + q.val + " is: " + lca.val);
+        System.out.println("你看到我,说明你更新成功了");
 
     }
 }
