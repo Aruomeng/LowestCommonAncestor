2e83f4f113a641ac5f088235087c042dec4516b8
Aruomeng <tyh043tyh@gmail.com>
2024-01-30 18:53:33 +0800

    "LowestCommonAncestor"

--- src/LowestCommonAncestor.java
+++ src/LowestCommonAncestor.java
@@ -57,6 +57,8 @@ public class LowestCommonAncestor {
 
         System.out.println("The Lowest Common Ancestor of " + p.val + " and " + q.val + " is: " + lca.val);
         System.out.println("你看到我,说明你更新成功了");
+        System.out.println("你看到我,说明你开发分支好哩");
+
 
     }
 }
